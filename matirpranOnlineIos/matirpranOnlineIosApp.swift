d4789890a6a7b79d99c495457effc5f6d0e8c2a9
rezaulkhan111 <rezaulkhan111@hotmail.com>
2026-05-01 22:49:14 +0600

    first commit

---
+++ matirpranOnlineIos/matirpranOnlineIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  matirpranOnlineIosApp.swift
+//  matirpranOnlineIos
+//
+//  Created by FAD on 1/5/26.
+//
+
+import SwiftUI
+
+@main
+struct matirpranOnlineIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ matirpranOnlineIosTests/matirpranOnlineIosTests.swift
@@ -0,0 +1,17 @@
+//
+//  matirpranOnlineIosTests.swift
+//  matirpranOnlineIosTests
+//
+//  Created by FAD on 1/5/26.
+//
+
+import Testing
+@testable import matirpranOnlineIos
+
+struct matirpranOnlineIosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
